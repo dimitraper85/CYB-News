291480bc15d5a4b997b557c3190f4d377f3ea739
Heena Varu <heena.nvr@gmail.com>
2024-07-17 14:28:03 +0200

    Add the tab selection functionality

--- app/javascript/controllers/tab_bar_controller.js
+++ app/javascript/controllers/tab_bar_controller.js
@@ -1,31 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="tab-bar"
 export default class extends Controller {
 
+  static targets = ["navLink"]
+
   connect() {
-    console.log("Connected...")
+    console.log("Tab bar controller connected...")
   }
 
-  addActiveClass(event) {
-    event.currentTarget.classList.add('active');
+  selectTab(event) {
+    event.preventDefault()
+    this.navLinkTargets.forEach(link => link.classList.remove("active"))
+    event.currentTarget.classList.add("active")
   }
 }
-
-
-
-// const tabBar = document.querySelector('.tab-bar');
-
-// // Add a click event listener to the entire tab bar
-// tabBar.addEventListener('click', function(event) {
-//   console.log("Listen click");
-//   // Check if the clicked element is a tab link
-//   if (event.currentTarget.classList.contains('tab-bar__nav-link')) {
-//     console.log("Remove tab class");
-//     // Remove the "active" class from all tab links
-//     const tabLinks = tabBar.querySelectorAll('.tab-bar__nav-link');
-//     tabLinks.forEach(link => link.classList.remove('active'));
-//     // Add the "active" class to the clicked link
-//     event.currentTarget.classList.add('active');
-//   }
-// });
