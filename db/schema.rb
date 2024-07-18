4c533ea4a48a8939cf7c80cc6f6a69e10f57168f
Max Mengeringhausen <max.mengeringhausen.jean@gmail.com>
2024-07-18 17:16:49 +0200

    migration user_articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_125139) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_151313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_18_125139) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "probability"
     t.index ["user_id"], name: "index_user_articles_on_user_id"
   end
 
