6bf31ebc23ba094d3b1468d10b2e401fc47cfe86
Dimitra Perissiou <dimitraper85@gmail.com>
2024-07-19 11:28:47 +0200

    maybe fixes favoritor issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import FavoritorController from "./controllers/favoritor_controller.js"
-Stimulus.register("favoritor", FavoritorController)
