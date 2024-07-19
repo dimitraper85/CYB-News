0d02da2b7d5aa6cb811b8af2495e3bc18dfe8e30
Heena Varu <heena.nvr@gmail.com>
2024-07-19 11:55:14 +0200

    Share with email is working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,16 +5,3 @@ import { application } from "controllers/application"
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
-
-//FIXME: - check if needed
-
-// import { Application } from "stimulus"
-// import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-// const application = Application.start()
-// const context = require.context("controllers", true, /_controller\.js$/)
-// application.load(definitionsFromContext(context))
