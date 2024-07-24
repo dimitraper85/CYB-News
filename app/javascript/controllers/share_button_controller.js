3adf552c883bd98a25580ab7472c0f2f474713ef
Heena Varu <heena.nvr@gmail.com>
2024-07-24 10:29:56 +0200

    Added functionality that when user clicks outside of the button the social share would dissapear

--- app/javascript/controllers/share_button_controller.js
+++ app/javascript/controllers/share_button_controller.js
@@ -2,14 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="share-button"
 export default class extends Controller {
-  static targets = [ "options" ]
+  static targets = [ "options", "button" ]
 
   connect() {
     console.log("share buttonnn connectedddddd")
+    // Add event listener to the document
+    document.addEventListener("click", this.hideOptionsOnClickOutside.bind(this));
   }
 
-  toggle() {
-    const arrayOptions = Array.from(this.optionsTarget.children)
-    this.optionsTarget.classList.toggle("active")
+  disconnect() {
+    // Remove event listener when the controller is disconnected
+    document.removeEventListener("click", this.hideOptionsOnClickOutside.bind(this));
+  }
+
+  toggle(event) {
+    event.stopPropagation(); // Prevent the click event from bubbling up to the document
+    this.optionsTarget.classList.toggle("active");
+  }
+
+  hideOptionsOnClickOutside(event) {
+    // console.log("Custom Button Target:", this.buttonTarget);
+    // console.log("Custom event.target:", event.target);
+
+    if (!this.buttonTarget.contains(event.target) && !this.optionsTarget.contains(event.target)) {
+      this.optionsTarget.classList.remove("active");
+    }
   }
 }
