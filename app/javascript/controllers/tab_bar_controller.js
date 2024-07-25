94c97f184bcf42f7339cd3154dbab72fba976883
Heena Varu <heena.nvr@gmail.com>
2024-07-25 12:14:03 +0200

    Remove unnecessry changes

--- app/javascript/controllers/tab_bar_controller.js
+++ app/javascript/controllers/tab_bar_controller.js
@@ -7,11 +7,11 @@ export default class extends Controller {
   connect() {
     console.log("Tab bar controller connected...")
     // Show the correct page based on the active tab link
-    // const activeLink = this.navLinkTargets.find((link) => link.classList.contains("active"))
-    // if (activeLink) {
-    //   const page = activeLink.dataset.page
-    //   this.showPage(page)
-    // }
+    const activeLink = this.navLinkTargets.find((link) => link.classList.contains("active"))
+    if (activeLink) {
+      const page = activeLink.dataset.page
+      this.showPage(page)
+    }
   }
 
   selectTab(event) {
